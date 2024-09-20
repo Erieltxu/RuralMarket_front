43fc610d308a942dd88139dbbc67b1c0c34d5689
Erieltxu <ld.aldecoa@gmail.com>
2024-09-20 11:17:36 +0200

    DONE: carousel

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+
+const Layout1 = () => {
+    return (
+        <div>
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+export default Layout1;
