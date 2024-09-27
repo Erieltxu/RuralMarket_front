6ad5bdd766e4a297c9de475ac4611546fb0ffa2f
Leire Del Hoyo <167010312+Erieltxu@users.noreply.github.com>
2024-09-27 11:08:02 +0200

    DONE: conflict resolved  TODO call back

--- src/components/frontPage/FrontPageCarousel.jsx
+++ src/components/frontPage/FrontPageCarousel.jsx
@@ -1,39 +1,25 @@
-import React, { useState } from 'react';
+import React from "react";
 
 const FrontPageCarousel = () => {
   const images = [
+    "/img/home4.png",
     "/img/home2.png",
     "/img/home3.png",
     "/img/home4.png"
   ];
 
-  const imagesPerPage = 3; // Controla cuántas imágenes mostrar a la vez
-  const totalImages = images.length;
-  const [currentPage, setCurrentPage] = useState(0);
-
-  // Función para ir a la siguiente página de imágenes
-  const handleNext = () => {
-    setCurrentPage((prevPage) => (prevPage + 1) % Math.ceil(totalImages / imagesPerPage));
-  };
-
-  // Función para ir a la página anterior de imágenes
-  const handlePrev = () => {
-    setCurrentPage((prevPage) => (prevPage - 1 + Math.ceil(totalImages / imagesPerPage)) % Math.ceil(totalImages / imagesPerPage));
-  };
-
-  // Calculamos el índice de las imágenes que se mostrarán en la página actual
-  const startIndex = currentPage * imagesPerPage;
-  const displayedImages = images.slice(startIndex, startIndex + imagesPerPage);
+  const imagesPerPage = 3;
 
   return (
     <div className="relative max-w-4xl mx-auto overflow-hidden w-full">
+      {/* Contenedor del carrusel */}
       <div className="flex justify-center space-x-4">
-        {displayedImages.map((image, index) => (
+        {images.slice(0, imagesPerPage).map((image, index) => (
           <div
             key={index}
             className="w-1/3 px-2 h-[300px] md:h-[350px] lg:h-[350px] rounded-[15px] relative group"
           >
-            <div className="relative overflow-hidden h-full rounded-lg border-2 border-transparent group-hover:border-green-500 transition-all duration-300 rounded-[15px]">
+            <div className="relative overflow-hidden h-full border-2 border-transparent group-hover:border-green-500 transition-all duration-300 rounded-[15px]">
               <img
                 src={image}
                 alt={`Slide ${index}`}
@@ -42,22 +28,6 @@ const FrontPageCarousel = () => {
             </div>
           </div>
         ))}
-      </div>
-
-      {/* Controles para navegar */}
-      <div className="flex justify-between mt-4">
-        <button
-          onClick={handlePrev}
-          className="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-lg"
-        >
-          Prev
-        </button>
-        <button
-          onClick={handleNext}
-          className="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-lg"
-        >
-          Next
-        </button>
       </div>
     </div>
   );
